caf65fc713d8bc1f08fcf17d1c9135cfa45224c5
Thaila Esteves <thailalestevesl@gmail.com>
2024-08-22 23:05:16 -0300

    simple airbnb show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Modern Loft in Downtown',
+  address: '123 Main Street',
+  description: 'A sleek, modern loft with city views and a minimalist design',
+  price_per_night: 120,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 90,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Beachfront Villa',
+  address: '456 Seaside Blvd, Beach Town',
+  description: 'A luxurious beachfront villa with direct ocean access.',
+  price_per_night: 300,
+  number_of_guests: 6
+)
